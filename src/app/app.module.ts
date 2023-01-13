dbd384166210c07fd6003f5c556c64b94e084d1d
Luis Miguel Fernández Costa <luismiguel.fernandez@murciaeduca.es>
2023-01-13 08:31:36 +0100

    ej02-hardware en marcha

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TiendaComponent } from './tienda/tienda.component';
+import { ArticuloComponent } from './articulo/articulo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TiendaComponent,
+    ArticuloComponent
   ],
   imports: [
     BrowserModule
